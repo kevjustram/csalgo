be71c473010177e73a7ea55c4f7b0940c5a97ee3
Kevin Ramirez <kevinjustin.ramirez@yahoo.com>
2016-09-17 19:46:02 +0800

    Fixed formatting

--- Practice/src/practice/SelectionSort.java
+++ Practice/src/practice/SelectionSort.java
@@ -1,42 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practice;
 
-/**
- *
- * @author ksramirez
- */
 public class SelectionSort {
-       public static void main(String[] args) {
-        int[] arr={8,0,2,1,5};
-        int temp=0;
+
+    public static void main(String[] args) {
+        int[] arr = {8, 0, 2, 1, 5};
+        int temp = 0;
         System.out.print("Array content = ");
-        for(int k=0;k<arr.length;k++){
-            System.out.print(arr[k]+" ");
+        for (int k = 0; k < arr.length; k++) {
+            System.out.print(arr[k] + " ");
         }
         System.out.println("");
-        for(int i=0;i<arr.length;i++){
-            int small=i;
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[j]<arr[small]){
-                    small=j;
+        for (int i = 0; i < arr.length; i++) {
+            int small = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[small]) {
+                    small = j;
                 }
             }
-            temp=arr[i];
-            arr[i]=arr[small];
-            arr[small]=temp;
-            for(int k=0;k<arr.length;k++){
-                System.out.print(arr[k]+" ");
+            temp = arr[i];
+            arr[i] = arr[small];
+            arr[small] = temp;
+            for (int k = 0; k < arr.length; k++) {
+                System.out.print(arr[k] + " ");
             }
             System.out.println("");
         }
         System.out.print("Ascending = ");
-        for(int k=0;k<arr.length;k++){
-            System.out.print(arr[k]+" ");
+        for (int k = 0; k < arr.length; k++) {
+            System.out.print(arr[k] + " ");
         }
-        
-    } 
+    }
 }
