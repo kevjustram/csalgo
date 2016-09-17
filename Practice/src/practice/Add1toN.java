1e4de5cdfd4c23f2e621e4d36d241d116d95d2ae
Kevin Ramirez <kevinjustin.ramirez@yahoo.com>
2016-09-17 19:46:20 +0800

    Fixed formatting

--- Practice/src/practice/Add1toN.java
+++ Practice/src/practice/Add1toN.java
@@ -2,25 +2,20 @@ package practice;
 
 import java.util.Scanner;
 
-/**
- *
- * @author ksramirez
- */
 public class Add1toN {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter a number to add from 1 to: ");
         int n = scan.nextInt();
-        System.out.println("Sum is: "+addition(n));
+        System.out.println("Sum is: " + addition(n));
     }
-    
-    public static int addition(int n){
-        if (n==1){
+
+    public static int addition(int n) {
+        if (n == 1) {
             return 1;
-        }
-        else{
-            return n+addition(n-1);
+        } else {
+            return n + addition(n - 1);
         }
     }
 }
