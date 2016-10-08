f3b0ef161483d7922c3cebf9ddafe346c6b0eb48
Kevin Ramirez <kevinjustin.ramirez@yahoo.com>
2016-10-08 21:04:00 +0800

    Fixed format

--- Exercise/src/Strassen/Strassen.java
+++ Exercise/src/Strassen/Strassen.java
@@ -16,15 +16,19 @@ public class Strassen {
         System.out.println("Input 4 number in Matrix A: ");
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
+                System.out.print("a[" + i + "]" + "[" + j + "]" + " = ");
                 a[i][j] = scan.nextInt();
             }
         }
+        System.out.println("");
         System.out.println("Input 4 number in Matrix B: ");
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
+                System.out.print("b[" + i + "]" + "[" + j + "]" + " = ");
                 b[i][j] = scan.nextInt();
             }
         }
+        System.out.println("");
         int m1 = (a[0][0] + a[1][1]) * (b[0][0] + b[1][1]);
         int m2 = (a[1][0] + a[1][1]) * b[0][0];
         int m3 = a[0][0] * (b[0][1] - b[1][1]);
@@ -36,10 +40,10 @@ public class Strassen {
         c[0][1] = m3 + m5;
         c[1][0] = m2 + m4;
         c[1][1] = m1 + m3 - m2 + m6;
-        System.out.println("A*B = ");
+        System.out.println("Matrix C(A*B): ");
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
-                System.out.println("c["+i+"]"+"["+j+"]"+" = "+c[i][j]);
+                System.out.println("c[" + i + "]" + "[" + j + "]" + " = " + c[i][j]);
             }
         }
     }
