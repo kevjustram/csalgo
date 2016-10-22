77121932fc616ab41e1a06974e4c460c11da80b9
Kevin Ramirez <kevinjustin.ramirez@yahoo.com>
2016-10-22 20:57:53 +0800

    Added Greedy and edited folders

---
+++ Greedy.java
@@ -0,0 +1,19 @@
+package Greedy;
+
+import java.util.Scanner;
+
+public class Greedy {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        double money = scan.nextDouble();
+        System.out.println("10 peso bills: " + (int) (money / 10));
+        money %= 10;
+        System.out.println("5 peso coins: " + (int) (money / 5));
+        money %= 5;
+        System.out.println("1 peso coins: " + (int) (money / 1));
+        money %= 1;
+        System.out.println("25 cent coins: " + (int) (money / .25));
+    }
+}
